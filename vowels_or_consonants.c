b2c86e525c3a105bb10cd84fcb3d1be376ec492f
ChandraVamsi06 <147473020+ChandraVamsi06@users.noreply.github.com>
2023-10-17 22:57:51 +0530

    Add files via upload

---
+++ vowels_or_consonants.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%c",&ch);
+	if(ch>=65 && ch<=90){
+		ch+=32;
+	}
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+		printf("Vowel");
+	}
+	else{
+		printf("Consonant");
+	}
+}
